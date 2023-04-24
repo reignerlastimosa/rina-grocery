5f49b1fce8a1474c479d9f30d0bf067dd8420555
Reigner Lastimosa <lastimosareignerc@gmail.com>
2023-04-25 00:43:14 +0800

    third version edited time limit and footer bug

--- src/pages/Cart/Checkout.jsx
+++ src/pages/Cart/Checkout.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 export const Checkout =() =>{
-    return(
-        <div className="checkoutPage">
-            <h1>Your groceries are now being prepared. Please wait for 15 mins</h1>
-        </div>
-    )
+    const [timeLeft, setTimeLeft] = useState(5 * 60); 
+    const [timeIsUp, setTimeIsUp] = useState(false); 
+  
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        setTimeLeft(prevTimeLeft => prevTimeLeft - 1);
+      }, 1000);
+  
+      return () => clearInterval(intervalId);
+    }, []);
+  
+    useEffect(() => {
+      if (timeLeft === 0) {
+        setTimeIsUp(true);
+      }
+    }, [timeLeft]);
+  
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
+  
+    return (
+      <div className="checkoutPage">
+        {timeIsUp ? (
+          <h1 className="time">You may now take your groceries at Rina Grocery Store. Thank you</h1>
+        ) : (
+          <div>
+            <h1>  Your groceries are now being prepared. Please wait for 5 minutes </h1>
+            <h2>Time left: {`${minutes}:${seconds < 10 ? '0' : ''}${seconds}`} </h2></div>
+          
+        )}
+        
+      </div>
+    );
 }
